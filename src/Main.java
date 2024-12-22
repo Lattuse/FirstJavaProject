f4c309beb1f3fa97c679bf93b3322f4bed769f31
Lattuse <sultik280805@gmail.com>
2024-12-22 17:56:19 +0500

    Adding new classes

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please type your name: ");
-        String name = input.nextLine();
-        System.out.println("Hello " + name + "!!!");
+
     }
 
 }
---
+++ src/SMP/Post.java
@@ -0,0 +1,4 @@
+package SMP;
+
+public class Post {
+}
